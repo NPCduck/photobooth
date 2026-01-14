ecf9011af44fbde91f407e3d513656102cf202b8
Patrik Kvasničák <patrikooor@gmail.com>
2026-01-14 01:02:04 +0100

    zaloha - súbory z local backupu

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        cors: true,
+        hmr: {
+            host: '10.10.10.33',
+            protocol: 'ws',
+        },
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
